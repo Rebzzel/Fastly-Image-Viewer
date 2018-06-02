bf78ce3c89ba757601d887eb02dc03563edf2866
Rebzzel <rebzzel@gmail.com>
2018-06-02 11:56:27 +0300

    Fix bug which didn't allow to show info window again

--- Fastly-Image-Viewer/InfoWindow.xaml.cs
+++ Fastly-Image-Viewer/InfoWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Fastly_Image_Viewer
 
         private void closeBtn_Click(object sender, RoutedEventArgs e)
         {
-            Close();
+            Hide();
         }
 
         private void githubLbl_Click(object sender, RoutedEventArgs e)
