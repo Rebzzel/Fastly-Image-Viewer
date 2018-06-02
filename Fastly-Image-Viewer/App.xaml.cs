12c0318761a6287264df1fdf6667ed61dc487692
Rebzzel <rebzzel@gmail.com>
2018-06-02 18:07:31 +0300

    Add single instance implementation

--- Fastly-Image-Viewer/App.xaml.cs
+++ Fastly-Image-Viewer/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Shell;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -8,7 +9,28 @@ using System.Windows;
 
 namespace Fastly_Image_Viewer
 {
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        [STAThread]
+        public static void Main()
+        {
+            if (SingleInstance<App>.InitializeAsFirstInstance("Fastly_Image_Viewer"))
+            {
+                App app = new App();
+
+                app.InitializeComponent();
+                app.Run();
+
+                SingleInstance<App>.Cleanup();
+            }
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            if (args.Count > 1)
+                (this.MainWindow as MainWindow).OpenImage(args[1]);
+
+            return true;
+        }
     }
 }
--- Fastly-Image-Viewer/MainWindow.xaml.cs
+++ Fastly-Image-Viewer/MainWindow.xaml.cs
@@ -29,8 +29,45 @@ namespace Fastly_Image_Viewer
         {
             InitializeComponent();
 
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+                this.OpenImage(args[1]);
+
             this.PickerWindow = new ColorPickerWindow();
             this.InfoWindow = new InfoWindow();
+        }
+
+        public void OpenImage(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException(path);
+
+            this.grid.Visibility = Visibility.Hidden;
+
+            this.Bitmap = new Bitmap(path);
+            MemoryStream stream = new MemoryStream();
+            this.Bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+            stream.Position = 0;
+
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.StreamSource = stream;
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.EndInit();
+
+            this.image.Width = bitmapImage.Width;
+            this.image.Height = bitmapImage.Height;
+            this.image.Source = bitmapImage;
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            this.infoLbl.Visibility = Visibility.Visible;
+            this.infoLbl2.Content = $"{fileInfo.Name}\n{String.Format("{0:f2}", (double)fileInfo.Length / 1024)} KB\n{Bitmap.Width} x {Bitmap.Height}";
+
+            this.saveAsBtn.IsEnabled = true;
+            this.zoomInBtn.IsEnabled = true;
+            this.zoomReloadBtn.IsEnabled = true;
+            this.zoomOutBtn.IsEnabled = true;
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -49,38 +86,12 @@ namespace Fastly_Image_Viewer
                 "All files (*.*)|*.*";
 
             if (dialog.ShowDialog().Value)
-            {
-                this.grid.Visibility = Visibility.Hidden;
-
-                this.Bitmap = new Bitmap(dialog.FileName);
-                MemoryStream stream = new MemoryStream();
-                this.Bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
-                stream.Position = 0;
-
-                BitmapImage bitmapImage = new BitmapImage();
-                bitmapImage.BeginInit();
-                bitmapImage.StreamSource = stream;
-                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-                bitmapImage.EndInit();
-
-                this.image.Width = bitmapImage.Width;
-                this.image.Height = bitmapImage.Height;
-                this.image.Source = bitmapImage;
-
-                this.infoLbl.Visibility = Visibility.Visible;
-                this.infoLbl2.Content = $"{dialog.SafeFileName}\n{String.Format("{0:f2}", (double)new FileInfo(dialog.FileName).Length / 1024)} KB\n{Bitmap.Width} x {Bitmap.Height}";
-
-                this.saveAsBtn.IsEnabled = true;
-                this.zoomInBtn.IsEnabled = true;
-                this.zoomReloadBtn.IsEnabled = true;
-                this.zoomOutBtn.IsEnabled = true;
-            }  
+                OpenImage(dialog.FileName);
         }
 
         private void saveAsBtn_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog dialog = new SaveFileDialog();
-            dialog.Filter =
 
             if (dialog.ShowDialog().Value)
             {
