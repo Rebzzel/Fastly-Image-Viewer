11814ae7decb28a33c10f70b98d92c17126b47bf
Rebzzel <rebzzel@gmail.com>
2018-06-08 00:04:48 +0300

    Update version

--- Fastly-Image-Viewer/Properties/Settings.Designer.cs
+++ Fastly-Image-Viewer/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Fastly_Image_Viewer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.11")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.04")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
